15247aa7b194608fa338f2e6c49c07c170fc9048
Peter Halloran <mkwenhua101@gmail.com>
2017-12-31 00:34:02 -0600

    added checks server checks in src/helpers/browser.js for SSR

--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -1,29 +1,35 @@
-
-var _isIE8 = !(document.createTextNode('test').textContent);
+if (typeof window !== "undefined") {
+  var _isIE8 = !(document.createTextNode('test').textContent);
+  var _isIE9 = !!(document.documentMode);
+  var _isSafari = (/Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor));
+  var _isChrome = (/Chrome/.test(navigator.userAgent) && /Google/.test(navigator.vendor));
+} else {
+  var _isIE8 = false;
+  var _isIE9 = false;
+  var _isSafari = false;
+  var _isChrome = true;
+}
 
 export function isIE8() {
   return _isIE8;
 }
 
-var _isIE9 = !!(document.documentMode);
-
 export function isIE9() {
   return _isIE9;
 }
 
-var _isSafari = (/Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor));
-
 export function isSafari() {
   return _isSafari;
 }
-
-var _isChrome = (/Chrome/.test(navigator.userAgent) && /Google/.test(navigator.vendor));
 
 export function isChrome() {
   return _isChrome;
 }
 
 export function isMobileBrowser(userAgent) {
+  if (typeof navigator === "undefined") {
+    return false;
+  }
   if (!userAgent) {
     userAgent = navigator.userAgent;
   }
